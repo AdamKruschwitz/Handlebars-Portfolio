e3245f0f5bdc3bfabc30d354e064a44b6680b932
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-11 21:26:25 -0700

    added scrollbar fix

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+// Override .nav-link function to account for scrolling with fixed navbar
+// Code modeled after code from https://newbedev.com/offset-scroll-anchor-in-html-with-bootstrap-4-fixed-navbar
+var divId;
+
+$('.nav-link').click(scrollToTop);
+
+function scrollToTop(){    
+    divId = $(this).attr('href');
+     $('html, body').animate({
+      scrollTop: $(divId).offset().top - 54
+    }, 100);
+}
