4742af1d7bf1cd70c0782d7248af58c24d495f64
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-07 18:18:26 -0800

    implemented controller for homepage

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const path = require('path');
 const sequelize = require('./config/connection');
+const Project = require('./models/Project');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,11 +17,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set routes
-app.get('/', function (req, res) {
-    res.render('home');
+app.get('/', async function (req, res) {
+    // Get data from DB
+    const featured = await Project.findAll({
+        where: {
+            featured: true
+        }
+    });
+    const all =  await Project.findAll();
+    console.log(featured, all);
+
+    // Parse data into simple JSON
+    const featuredJSON = featured.map(project => project.get({ plain: true }));
+    const allJSON = featured.map(project => project.get({ plain: true }));
+    
+    // Render the page
+    res.render('home', { featured: featuredJSON, projects: allJSON });
 });
 
 // Initialize DB, then start the server
-sequelize.sync({ force: true }).then(() =>{
+sequelize.sync({ force: false }).then(() =>{
     app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}.`));
 });
